1747a25d1c11f4122bf77dbd4cd700bbf388aa3c
Johnson Chandra <johnson.chandra@gmail.com>
2021-06-15 15:54:40 +0700

    meteor npm update n audit fix --force, add mode jit in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
+  mode: 'jit',
   purge: ['./apps/**/*.{js,jsx,ts,tsx}', './public/*.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
