b51d8ae583935791e6806a785f8989263ed85fee
Johnson Chandra <johnson.chandra@gmail.com>
2021-06-16 13:48:38 +0700

    bugfix parseMemberFromContext.js

--- apps/common/helpers/parseMemberFromContext.js
+++ apps/common/helpers/parseMemberFromContext.js
@@ -3,12 +3,8 @@ export default (context) => {
     _id: (context.user && context.user._id) || 'Anonymous',
     type: context.user && context.user._id ? 'Member' : 'Anonymous',
     name:
-      context.user &&
-      context.user &&
-      context.user &&
-      context.user.profile &&
-      context.user.profile.fullname
-        ? 'Member'
+      context.user && context.user.profile && context.user.profile.fullname
+        ? context.user.profile.fullname
         : 'Anonymous',
   };
 };
